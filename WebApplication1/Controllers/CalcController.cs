f248625b4065bc60609e1e568070210b1a2fa2c8
Diana Joselin Lopez <Diana Joselin Lopez@DESKTOP-1EUKP7N>
2021-06-29 12:02:24 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication1/Controllers/CalcController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Index()
+        {
+            return View(new Class1());
+        }
+        [ HttpPost]
+        public ActionResult Index(Class1 c, string calcular)
+        {
+            if (calcular == "1")
+            {
+                c.total = c.n1 + c.n2;
+
+            }
+            else if (calcular == "2")
+            {
+                c.total = c.n1 - c.n2;
+
+            }
+            else if (calcular == "3")
+            {
+                c.total = c.n1 * c.n2;
+
+            }
+            else
+            {
+                c.total = c.n1 / c.n2;
+
+            }
+            return View(c);
+
+
+        }
+
+    }
+  }
---
+++ WebApplication1/Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Class1
+    {
+        public int n1 { get; set; }
+        public int n2 { get; set; }
+        public int total { get; set; }
+    }
+}
